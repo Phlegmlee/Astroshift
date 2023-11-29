6eb4ed55b56979f3169b964be68990b6384a9183
Tanner <152333980+Phlegmlee@users.noreply.github.com>
2023-11-29 17:14:31 -0600

    Create Title Page and Links

---
+++ Title Page/Level_Select.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _on_new_game_button_pressed():
+	get_tree().change_scene_to_file("res://Levels/Level_1_Normal.tscn")
+
+
+func _on_new_game_plus_button_pressed():
+	pass # Replace with function body.
