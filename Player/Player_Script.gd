9328b5c2042a603e08d708c40d9ac7a27e4ac14a
Tanner <152333980+Phlegmlee@users.noreply.github.com>
2023-12-01 21:55:41 -0600

    Finished basic framework additions

--- Player/Player_Script.gd
+++ Player/Player_Script.gd
@@ -1,40 +1,77 @@
 extends CharacterBody2D
 
-
+var playerHealth = 20
 const SPEED = 300.0
 const JUMP_VELOCITY = -450.0
+const powerSPEED = 350.0
+const powerJUMP = -500.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 @onready var anim = get_node("AnimationPlayer")
+
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("player_jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		anim.play("Jump")
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("player_left", "player_right")
-	if direction == -1:
-		get_node("AnimatedSprite2D").flip_h = true
-	elif direction == 1:
-		get_node("AnimatedSprite2D").flip_h = false
 		
+	if GlobalVariables.powerup == false:
 	
-	if direction:
-		velocity.x = direction * SPEED
-		if velocity.y == 0:
-			anim.play("Run")
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		anim.play("Idle")
-		if velocity.y > 0:
-			anim.play("Fall")
+		# Handle Jump.
+		if Input.is_action_just_pressed("player_jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			anim.play("BaseAnimations/Jump")
+		
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction = Input.get_axis("player_left", "player_right")
+		if direction == -1:
+			get_node("Astro").flip_h = true
+		elif direction == 1:
+			get_node("Astro").flip_h = false
+			
+		
+		if direction:
+			velocity.x = direction * SPEED
+			if velocity.y == 0:
+				anim.play("BaseAnimations/Run")
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			anim.play("BaseAnimations/Idle")
+			if velocity.y > 0:
+				anim.play("BaseAnimations/Fall")
 
+	if GlobalVariables.powerup == true:
+			# Handle Jump.
+		if Input.is_action_just_pressed("player_jump") and is_on_floor():
+			velocity.y = powerJUMP
+			anim.play("PowerAnimations/PowerJump")
+		
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction = Input.get_axis("player_left", "player_right")
+		if direction == -1:
+			get_node("Astro").flip_h = true
+		elif direction == 1:
+			get_node("Astro").flip_h = false
+			
+		
+		if direction:
+			velocity.x = direction * powerSPEED
+			if velocity.y == 0:
+				anim.play("PowerAnimations/PowerRun")
+		else:
+			velocity.x = move_toward(velocity.x, 0, powerSPEED)
+			anim.play("PowerAnimations/PowerIdle")
+			if velocity.y > 0:
+				anim.play("PowerAnimations/PowerFall")
+
+	
 	move_and_slide()
+
+	if playerHealth <= 0:
+		print("Player Death")
+		queue_free()
+		get_tree().change_scene_to_file("res://Levels/Level_1_Normal.tscn")
--- Title Page/Level_Select.gd
+++ Title Page/Level_Select.gd
@@ -4,5 +4,5 @@ func _on_new_game_button_pressed():
 	get_tree().change_scene_to_file("res://Levels/Level_1_Normal.tscn")
 
 
-func _on_new_game_plus_button_pressed():
-	pass # Replace with function body.
+func _on_load_game_pressed():
+	Utilities.loadGame()
