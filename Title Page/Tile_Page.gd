c6d8fa3d38358400c9f74a1e699caeb727f93236
Tanner <152333980+Phlegmlee@users.noreply.github.com>
2023-12-05 20:20:21 -0600

    Update Tile_Page.gd

--- Title Page/Tile_Page.gd
+++ Title Page/Tile_Page.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _ready():
 	MusicController.play_music(0, -8.0, 0.5)
-	SFX.play_sfx(0, 1.5, 0.5)
+	SFX.play_sfx(0, 1.5, 0.5, 1)
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://Title Page/LevelSelect.tscn")
